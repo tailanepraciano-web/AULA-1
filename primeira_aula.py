36a84a174c0bcd6736b6c915db468698fa8cb571
tailanepraciano-web <tailanepraciano@gmail.com>
2026-01-07 19:06:45 -0300

    Add files via upload

---
+++ primeira_aula.py
@@ -0,0 +1,48 @@
+#Imprimir "Olá, Tay"
+
+#nome_variavel Snake Case
+#nomeVariavel Camel Case
+
+#print(f'Olá {nome}!')
+#print(f"Seja bem-vinda {nome}")
+# #print(f"Olá, {nome}!")
+# #print("Olá,",nome+"!")
+
+
+
+# # print(f"Olá, meu nome é {nome}! Tenho {idade} anos e {altura} metros")
+
+# num1= 58
+# num2 = 158
+
+# soma = num1 + num2
+# subtracao = num1 - num2
+# multiplicacao = num1 * num2
+# divisao = num1 / num2
+
+# print(f'''
+#       resultados:
+      
+#             soma: {soma}
+#             subtracao:{subtracao}
+#             multiplicacao: {multiplicacao}
+#             divisao: {divisao}      
+#       ''')
+
+nome = input("Digite seu nome:")
+idade = int(input("Digite sua idade:"))
+altura =float(input("Digite sua altura:"))
+ano_atual = 2025
+
+Data_nascimento = ano_atual - idade
+
+
+print(f"Eu nasci no ano {Data_nascimento}")
+
+
+
+
+
+
+
+
